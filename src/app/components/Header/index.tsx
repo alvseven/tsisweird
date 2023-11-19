96b60cc0f4a8cb012e1a1ec0a823f0d1534c5ed9
root <root@alv>
2023-11-19 04:34:25 -0300

    move logo in header from right to left

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -10,7 +10,7 @@ export function Header({ isPlaying }: HeaderProps) {
     return <header className="w-full h-14 flex items-center justify-center p-4 bg-gradient-to-r from-[#04162f] via-[#10132B] to-[#071c3b] shadow-blue-950 shadow-sm drop-shadow-lg animate-pulse">
         <nav className="w-full">
             <ul className="flex items-center justify-center w-full gap-8">
-                <li className="absolute sm:right-8 md:right-12">
+                <li className="absolute sm:left-8 md:left-12">
                     <Link href="https://twitter.com/typescripting" target="_blank" className="brightness-200 animate-pulse">
                         <Image
                             src={Logo}
