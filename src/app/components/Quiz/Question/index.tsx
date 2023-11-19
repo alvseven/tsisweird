73291d39757d24127736656bb345e6fce00864e1
root <root@alv>
2023-11-19 01:59:03 -0300

    create current question tracker

--- src/app/components/Quiz/Question/index.tsx
+++ src/app/components/Quiz/Question/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Question1 from "../../../../../public/question1.svg"
 
 export function Question() {
-    return <section className="flex flex-col gap-8 drop-shadow-lg shadow-lg shadow-blue-950 pb-12 px-12">
+    return <section className="flex flex-col gap-9 drop-shadow-lg shadow-lg shadow-blue-950 pb-12 px-12">
         <div className="flex flex-col items-center justify-center">
             <h2 className="font-inter text-xl text-center">What is the output of <span className="font-roboto-mono text-[#1AD6B5]">UserWithoutName?</span></h2>
             <Image src={Question1} alt="first question" />
@@ -12,7 +12,7 @@ export function Question() {
                 Error: {"Type 'namee' does not satisfy the constraint 'name'"}
             </button>
             <button className="rounded-2xl text-slate-100 truncate font-inter border-2 border-dotted border-blue-800 p-4 text-xs hover:border-blue-800 hover:border-solid">
-                Same as User
+                {"{ name: string; age: number; }"}
             </button>
             <button className="rounded-2xl text-slate-100 truncate font-inter border-2 border-dotted border-blue-800 p-4 text-xs hover:border-blue-800 hover:border-solid">
                 {"{}"}
@@ -20,6 +20,9 @@ export function Question() {
             <button className="rounded-2xl text-slate-100 truncate font-inter border-2 border-dotted border-blue-800 p-4 text-xs hover:border-blue-800 hover:border-solid">
                 {"{ age: number; }"}
             </button>
+        </div>
+        <div className="flex items-center justify-center">
+            <h4 className="text-sm font-inter">Question 1 of 15</h4>
         </div>
     </section>
 }
